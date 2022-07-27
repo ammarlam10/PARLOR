66e3b0b75679c780572ec29becd68933ca81ea94
Ammar <ammarlam10@gmail.com>
2022-07-27 21:52:45 +0200

    bert

---
+++ BERT_LDA.py
@@ -0,0 +1,28 @@
+import numpy as np # linear algebra
+import os
+import json
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
+from nltk.corpus import wordnet
+import re
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords 
+
+import gensim
+from gensim.utils import simple_preprocess
+from gensim import corpora, models
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+
+import datetime
+import time
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import nltk

a234020a651f48d461e30f409cda71fb89583316
Ammar <ammarlam10@gmail.com>
2022-07-28 16:06:14 +0200

    bert

--- remove_dup.py
+++ remove_dup.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 
+path = '../monthly/post_201903.csv'
 
-df = pd.read_csv('../monthly/post_201903.csv')
+df = pd.read_csv(path)
 
 print('With duplicates', df.shape)
 
 df = df.drop_duplicates()
 
 print('no duplicates',df.shape)
+
+df.to_csv(path, index=False)

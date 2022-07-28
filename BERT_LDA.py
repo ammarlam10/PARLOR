4bf2375514c30c409138f9698f4bbbbdce62cf2d
Ammar <ammarlam10@gmail.com>
2022-07-28 15:27:24 +0200

    bert

--- BERT_LDA.py
+++ BERT_LDA.py
@@ -32,14 +32,328 @@ import nltk
 from collections import Counter
 from sklearn.metrics import silhouette_score
 import umap
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from gensim.models.coherencemodel import CoherenceModel
-import numpy as np
-import os
 
+
+
+from stop_words import get_stop_words
+from nltk.stem.porter import PorterStemmer
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+# from language_detector import detect_language
 
 
 meta = pd.read_csv('../monthly/post_201903.csv')
 
 print(meta.shape)
+
+
+def get_topic_words(token_lists, labels, k=None):
+    """
+    get top words within each topic from clustering results
+    """
+    if k is None:
+        k = len(np.unique(labels))
+    topics = ['' for _ in range(k)]
+    for i, c in enumerate(token_lists):
+        topics[labels[i]] += (' ' + ' '.join(c))
+    word_counts = list(map(lambda x: Counter(x.split()).items(), topics))
+    # get sorted word counts
+    word_counts = list(map(lambda x: sorted(x, key=lambda x: x[1], reverse=True), word_counts))
+    # get topics
+    topics = list(map(lambda x: list(map(lambda x: x[0], x[:10])), word_counts))
+
+    return topics
+
+def get_coherence(model, token_lists, measure='c_v'):
+    """
+    Get model coherence from gensim.models.coherencemodel
+    :param model: Topic_Model object
+    :param token_lists: token lists of docs
+    :param topics: topics as top words
+    :param measure: coherence metrics
+    :return: coherence score
+    """
+    if model.method == 'LDA':
+        cm = CoherenceModel(model=model.ldamodel, texts=token_lists, corpus=model.corpus, dictionary=model.dictionary,
+                            coherence=measure)
+    else:
+        topics = get_topic_words(token_lists, model.cluster_model.labels_)
+        cm = CoherenceModel(topics=topics, texts=token_lists, corpus=model.corpus, dictionary=model.dictionary,
+                            coherence=measure)
+    return cm.get_coherence()
+
+def get_silhouette(model):
+    """
+    Get silhouette score from model
+    :param model: Topic_Model object
+    :return: silhouette score
+    """
+    if model.method == 'LDA':
+        return
+    lbs = model.cluster_model.labels_
+    vec = model.vec[model.method]
+    return silhouette_score(vec, lbs)
+
+def plot_proj(embedding, lbs):
+    """
+    Plot UMAP embeddings
+    :param embedding: UMAP (or other) embeddings
+    :param lbs: labels
+    """
+    n = len(embedding)
+    counter = Counter(lbs)
+    for i in range(len(np.unique(lbs))):
+        plt.plot(embedding[:, 0][lbs == i], embedding[:, 1][lbs == i], '.', alpha=0.5,
+                 label='cluster {}: {:.2f}%'.format(i, counter[i] / n * 100))
+    plt.legend(loc = 'best')
+    plt.grid(color ='grey', linestyle='-',linewidth = 0.25)
+
+
+def visualize(model):
+    """
+    Visualize the result for the topic model by 2D embedding (UMAP)
+    :param model: Topic_Model object
+    """
+    if model.method == 'LDA':
+        return
+    reducer = umap.UMAP()
+    print('Calculating UMAP projection ...')
+    vec_umap = reducer.fit_transform(model.vec[model.method])
+    print('Calculating UMAP projection. Done!')
+    plot_proj(vec_umap, model.cluster_model.labels_)
+    dr = '../contextual_topic_identification/docs/images/{}/{}'.format(model.method, model.id)
+    if not os.path.exists(dr):
+        os.makedirs(dr)
+    plt.savefig('../2D_vis')
+
+def get_wordcloud(model, token_lists, topic):
+    """
+    Get word cloud of each topic from fitted model
+    :param model: Topic_Model object
+    :param sentences: preprocessed sentences from docs
+    """
+    if model.method == 'LDA':
+        return
+    print('Getting wordcloud for topic {} ...'.format(topic))
+    lbs = model.cluster_model.labels_
+    tokens = ' '.join([' '.join(_) for _ in np.array(token_lists)[lbs == topic]])
+
+    wordcloud = WordCloud(width=800, height=560,
+                          background_color='white', collocations=False,
+                          min_font_size=10).generate(tokens)
+
+    # plot the WordCloud image
+    plt.figure(figsize=(8, 5.6), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    dr = '../{}/{}'.format(model.method, model.id)
+    if not os.path.exists(dr):
+        os.makedirs(dr)
+    plt.savefig('../' + '/Topic' + str(topic) + '_wordcloud')
+    print('Getting wordcloud for topic {}. Done!'.format(topic))
+
+
+# Preprocessing
+
+
+# from stop_words import get_stop_words
+# from nltk.stem.porter import PorterStemmer
+# import re
+# import nltk
+# from nltk.tokenize import word_tokenize
+# from language_detector import detect_language
+
+# import pkg_resources
+# from symspellpy import SymSpell, Verbosity
+
+# sym_spell = SymSpell(max_dictionary_edit_distance=3, prefix_length=7)
+# dictionary_path = pkg_resources.resource_filename(
+#     "symspellpy", "frequency_dictionary_en_82_765.txt")
+# if sym_spell.word_count:
+#     pass
+# else:
+#     sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+
+
+###################################
+#### sentence level preprocess ####
+###################################
+
+# lowercase + base filter
+# some basic normalization
+def f_base(s):
+    """
+    :param s: string to be processed
+    :return: processed string: see comments in the source code for more info
+    """
+    # normalization 1: xxxThis is a --> xxx. This is a (missing delimiter)
+    s = re.sub(r'([a-z])([A-Z])', r'\1\. \2', s)  # before lower case
+    # normalization 2: lower case
+    s = s.lower()
+    # normalization 3: "&gt", "&lt"
+    s = re.sub(r'&gt|&lt', ' ', s)
+    # normalization 4: letter repetition (if more than 2)
+    s = re.sub(r'([a-z])\1{2,}', r'\1', s)
+    # normalization 5: non-word repetition (if more than 1)
+    s = re.sub(r'([\W+])\1{1,}', r'\1', s)
+    # normalization 6: string * as delimiter
+    s = re.sub(r'\*|\W\*|\*\W', '. ', s)
+    # normalization 7: stuff in parenthesis, assumed to be less informal
+    # s = re.sub(r'\(.*?\)', '. ', s)
+    # normalization 8: xxx[?!]. -- > xxx.
+    s = re.sub(r'\W+?\.', '.', s)
+    # normalization 9: [.?!] --> [.?!] xxx
+    s = re.sub(r'(\.|\?|!)(\w)', r'\1 \2', s)
+    # normalization 10: ' ing ', noise text
+    # s = re.sub(r' ing ', ' ', s)
+    # normalization 11: noise text
+    # s = re.sub(r'product received for free[.| ]', ' ', s)
+    # normalization 12: phrase repetition
+    s = re.sub(r'(.{2,}?)\1{1,}', r'\1', s)
+
+    return s.strip()
+
+
+# language detection
+
+# def f_lan(s):
+#     """
+#     :param s: string to be processed
+#     :return: boolean (s is English)
+#     """
+
+#     # some reviews are actually english but biased toward french
+#     return detect_language(s) in {'English', 'French','Spanish','Chinese'}
+
+
+###############################
+#### word level preprocess ####
+###############################
+
+# filtering out punctuations and numbers
+def f_punct(w_list):
+    """
+    :param w_list: word list to be processed
+    :return: w_list with punct and number filter out
+    """
+    return [word for word in w_list if word.isalpha()]
+
+
+# selecting nouns
+def f_noun(w_list):
+    """
+    :param w_list: word list to be processed
+    :return: w_list with only nouns selected
+    """
+    return [word for (word, pos) in nltk.pos_tag(w_list) if pos[:2] == 'NN']
+
+
+# typo correction
+def f_typo(w_list):
+    """
+    :param w_list: word list to be processed
+    :return: w_list with typo fixed by symspell. words with no match up will be dropped
+    """
+    w_list_fixed = []
+    for word in w_list:
+        suggestions = sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=3)
+        if suggestions:
+            w_list_fixed.append(suggestions[0].term)
+        else:
+            pass
+            # do word segmentation, deprecated for inefficiency
+            # w_seg = sym_spell.word_segmentation(phrase=word)
+            # w_list_fixed.extend(w_seg.corrected_string.split())
+    return w_list_fixed
+
+
+# stemming if doing word-wise
+p_stemmer = PorterStemmer()
+
+
+def f_stem(w_list):
+    """
+    :param w_list: word list to be processed
+    :return: w_list with stemming
+    """
+    return [p_stemmer.stem(word) for word in w_list]
+
+
+# filtering out stop words
+# create English stop words list
+
+stop_words = (list(
+    set(get_stop_words('en'))
+    # |set(get_stop_words('es'))
+    # |set(get_stop_words('de'))
+    # |set(get_stop_words('it'))
+    # |set(get_stop_words('ca'))
+    # #|set(get_stop_words('cy'))
+    # |set(get_stop_words('pt'))
+    # #|set(get_stop_words('tl'))
+    # |set(get_stop_words('pl'))
+    # #|set(get_stop_words('et'))
+    # |set(get_stop_words('da'))
+    # |set(get_stop_words('ru'))
+    # #|set(get_stop_words('so'))
+    # |set(get_stop_words('sv'))
+    # |set(get_stop_words('sk'))
+    # #|set(get_stop_words('cs'))
+    # |set(get_stop_words('nl'))
+    #|set(get_stop_words('sl'))
+    #|set(get_stop_words('no'))
+    #|set(get_stop_words('zh-cn'))
+))
+
+
+
+
+
+def f_stopw(w_list):
+    """
+    filtering out stop words
+    """
+    return [word for word in w_list if word not in stop_words]
+
+
+def preprocess_sent(rw):
+    """
+    Get sentence level preprocessed data from raw review texts
+    :param rw: review to be processed
+    :return: sentence level pre-processed review
+    """
+    s = f_base(rw)
+    if not f_lan(s):
+        return None
+    return s
+
+
+def preprocess_word(s):
+    """
+    Get word level preprocessed data from preprocessed sentences
+    including: remove punctuation, select noun, fix typo, stem, stop_words
+    :param s: sentence to be processed
+    :return: word level pre-processed review
+    """
+    if not s:
+        return None
+    w_list = word_tokenize(s)
+    w_list = f_punct(w_list)
+    w_list = f_noun(w_list)
+    w_list = f_typo(w_list)
+    w_list = f_stem(w_list)
+    w_list = f_stopw(w_list)
+
+    return w_list
+
+
+import keras
+from keras.layers import Input, Dense
+from keras.models import Model
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings('ignore')

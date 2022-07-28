a4ba6f6de0e6343f52fc836f619c8c5724ff34c7
Ammar <ammarlam10@gmail.com>
2022-07-28 23:25:18 +0200

    bert

--- BERT_LDA.py
+++ BERT_LDA.py
@@ -49,6 +49,8 @@ meta = pd.read_csv('../monthly/post_201903.csv')
 
 print(meta.shape)
 
+documents = meta
+
 
 def get_topic_words(token_lists, labels, k=None):
     """
@@ -409,3 +411,232 @@ import numpy as np
 #from Autoencoder import *
 #from preprocess import *
 from datetime import datetime
+
+
+def preprocess(docs, samp_size=df.shape[0]//2):
+    """
+    Preprocess the data
+    """
+    if not samp_size:
+        samp_size = 100
+
+    print('Preprocessing raw texts ...')
+    n_docs = len(docs)
+    sentences = []  # sentence level preprocessed
+    token_lists = []  # word level preprocessed
+    idx_in = []  # index of sample selected
+    #     samp = list(range(100))
+    samp = np.random.choice(n_docs, samp_size)
+    for i, idx in enumerate(samp):
+        sentence = preprocess_sent(docs[idx])
+        token_list = preprocess_word(sentence)
+        if token_list:
+            idx_in.append(idx)
+            sentences.append(sentence)
+            token_lists.append(token_list)
+        print('{} %'.format(str(np.round((i + 1) / len(samp) * 100, 2))), end='\r')
+    print('Preprocessing raw texts. Done!')
+    return sentences, token_lists, idx_in
+
+
+# define model object
+class Topic_Model:
+    def __init__(self, k=10, method='TFIDF'):
+        """
+        :param k: number of topics
+        :param method: method chosen for the topic model
+        """
+        if method not in {'TFIDF', 'LDA', 'BERT', 'LDA_BERT'}:
+            raise Exception('Invalid method!')
+        self.k = k
+        self.dictionary = None
+        self.corpus = None
+        #         self.stopwords = None
+        self.cluster_model = None
+        self.ldamodel = None
+        self.vec = {}
+        self.gamma = 15  # parameter for reletive importance of lda
+        self.method = method
+        self.AE = None
+        self.id = method + '_' + datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+    def vectorize(self, sentences, token_lists, method=None):
+        """
+        Get vecotr representations from selected methods
+        """
+        # Default method
+        if method is None:
+            method = self.method
+
+        # turn tokenized documents into a id <-> term dictionary
+        self.dictionary = corpora.Dictionary(token_lists)
+        # convert tokenized documents into a document-term matrix
+        self.corpus = [self.dictionary.doc2bow(text) for text in token_lists]
+
+        if method == 'TFIDF':
+            print('Getting vector representations for TF-IDF ...')
+            tfidf = TfidfVectorizer()
+            vec = tfidf.fit_transform(sentences)
+            print('Getting vector representations for TF-IDF. Done!')
+            return vec
+
+        elif method == 'LDA':
+            print('Getting vector representations for LDA ...')
+            if not self.ldamodel:
+                self.ldamodel = gensim.models.ldamodel.LdaModel(self.corpus, num_topics=self.k, id2word=self.dictionary,
+                                                                passes=20)
+
+            def get_vec_lda(model, corpus, k):
+                """
+                Get the LDA vector representation (probabilistic topic assignments for all documents)
+                :return: vec_lda with dimension: (n_doc * n_topic)
+                """
+                n_doc = len(corpus)
+                vec_lda = np.zeros((n_doc, k))
+                for i in range(n_doc):
+                    # get the distribution for the i-th document in corpus
+                    for topic, prob in model.get_document_topics(corpus[i]):
+                        vec_lda[i, topic] = prob
+
+                return vec_lda
+
+            vec = get_vec_lda(self.ldamodel, self.corpus, self.k)
+            print('Getting vector representations for LDA. Done!')
+            return vec
+
+        elif method == 'BERT':
+
+            print('Getting vector representations for BERT ...')
+            from sentence_transformers import SentenceTransformer
+            model = SentenceTransformer('bert-base-nli-max-tokens')
+            vec = np.array(model.encode(sentences, show_progress_bar=True))
+            print('Getting vector representations for BERT. Done!')
+            return vec
+
+             
+        elif method == 'LDA_BERT':
+        #else:
+            vec_lda = self.vectorize(sentences, token_lists, method='LDA')
+            vec_bert = self.vectorize(sentences, token_lists, method='BERT')
+            vec_ldabert = np.c_[vec_lda * self.gamma, vec_bert]
+            self.vec['LDA_BERT_FULL'] = vec_ldabert
+            if not self.AE:
+                self.AE = Autoencoder()
+                print('Fitting Autoencoder ...')
+                self.AE.fit(vec_ldabert)
+                print('Fitting Autoencoder Done!')
+            vec = self.AE.encoder.predict(vec_ldabert)
+            return vec
+
+    def fit(self, sentences, token_lists, method=None, m_clustering=None):
+        """
+        Fit the topic model for selected method given the preprocessed data
+        :docs: list of documents, each doc is preprocessed as tokens
+        :return:
+        """
+        # Default method
+        if method is None:
+            method = self.method
+        # Default clustering method
+        if m_clustering is None:
+            m_clustering = KMeans
+
+        # turn tokenized documents into a id <-> term dictionary
+        if not self.dictionary:
+            self.dictionary = corpora.Dictionary(token_lists)
+            # convert tokenized documents into a document-term matrix
+            self.corpus = [self.dictionary.doc2bow(text) for text in token_lists]
+
+        ####################################################
+        #### Getting ldamodel or vector representations ####
+        ####################################################
+
+        if method == 'LDA':
+            if not self.ldamodel:
+                print('Fitting LDA ...')
+                self.ldamodel = gensim.models.ldamodel.LdaModel(self.corpus, num_topics=self.k, id2word=self.dictionary,
+                                                                passes=20)
+                print('Fitting LDA Done!')
+        else:
+            print('Clustering embeddings ...')
+            self.cluster_model = m_clustering(self.k)
+            self.vec[method] = self.vectorize(sentences, token_lists, method)
+            self.cluster_model.fit(self.vec[method])
+            print('Clustering embeddings. Done!')
+
+    def predict(self, sentences, token_lists, out_of_sample=None):
+        """
+        Predict topics for new_documents
+        """
+        # Default as False
+        out_of_sample = out_of_sample is not None
+
+        if out_of_sample:
+            corpus = [self.dictionary.doc2bow(text) for text in token_lists]
+            if self.method != 'LDA':
+                vec = self.vectorize(sentences, token_lists)
+                print(vec)
+        else:
+            corpus = self.corpus
+            vec = self.vec.get(self.method, None)
+
+        if self.method == "LDA":
+            lbs = np.array(list(map(lambda x: sorted(self.ldamodel.get_document_topics(x),
+                                                     key=lambda x: x[1], reverse=True)[0][0],
+                                    corpus)))
+        else:
+            lbs = self.cluster_model.predict(vec)
+        return lbs
+
+
+
+#from model import *
+#from utils import *
+
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore', category=Warning)
+
+import argparse
+
+#def model(): #:if __name__ == '__main__':
+
+def main():
+    
+    
+    method = "LDA_BERT"
+    samp_size = documents.shape[0]//2
+    ntopic = 10
+    
+    #parser = argparse.ArgumentParser(description='contextual_topic_identification tm_test:1.0')
+
+    #parser.add_argument('--fpath', default='/kaggle/working/train.csv')
+    #parser.add_argument('--ntopic', default=10,)
+    #parser.add_argument('--method', default='TFIDF')
+    #parser.add_argument('--samp_size', default=20500)
+    
+    #args = parser.parse_args()
+
+    data = documents['body'] #pd.read_csv('/kaggle/working/train.csv')
+    data = data.fillna('')  # only the comments has NaN's
+    rws = data.abstract
+    sentences, token_lists, idx_in = preprocess(rws, samp_size=samp_size)
+    # Define the topic model object
+    #tm = Topic_Model(k = 10), method = TFIDF)
+    tm = Topic_Model(k = ntopic, method = method)
+    # Fit the topic model by chosen method
+    tm.fit(sentences, token_lists)
+    # Evaluate using metrics
+    with open("../monthly/comment_{}.file".format(tm.id), "wb") as f:
+        pickle.dump(tm, f, pickle.HIGHEST_PROTOCOL)
+
+    print('Coherence:', get_coherence(tm, token_lists, 'c_v'))
+    print('Silhouette Score:', get_silhouette(tm))
+    # visualize and save img
+    visualize(tm)
+    for i in range(tm.k):
+        get_wordcloud(tm, token_lists, i)
+        

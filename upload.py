34a6fe90e3d64e474aa3cbb9787c576d64ecb61d
Ammar <ammarlam10@gmail.com>
2022-08-12 12:34:20 +0200

    swin

--- upload.py
+++ upload.py
@@ -6,7 +6,7 @@ from pydrive.drive import GoogleDrive
 
 upload_file_list = ['comment_202002.csv', 'post_201912.csv']
 for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'parents': [{'id': '1dZXmRPPr6Z8C9OqixmLxW-pcMIW4p7FA'}]})
+	gfile = GoogleDrive.CreateFile({'parents': [{'id': '1dZXmRPPr6Z8C9OqixmLxW-pcMIW4p7FA'}]})
 	# Read file and set it as the content of this instance.
 	gfile.SetContentFile(upload_file)
 	gfile.Upload() # Upload the file.

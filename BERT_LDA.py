59d2200e68614b9b4ef64a4a38f6cc7d4f16826c
Ammar <ammarlam10@gmail.com>
2022-08-17 14:39:39 +0200

    swin

--- BERT_LDA.py
+++ BERT_LDA.py
@@ -615,7 +615,7 @@ def main():
 # '201901.csv','201902.csv',
 # '201903.csv','201904.csv',      '201905.csv',
     onlyfiles = ['201906.csv','201907.csv','201908.csv','201909.csv','201910.csv']
-
+#    onlyfiles = [01911  201912  202001  202002  202003  202004]
 
     for fil in onlyfiles:
         print(fil)
